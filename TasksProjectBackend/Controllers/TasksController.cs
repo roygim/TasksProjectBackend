8e4fbb40765b3a36467bd0118daf64e41cfce9cd
roygim <roygim@gmail.com>
2023-11-13 11:24:43 +0200

    Add project files.

---
+++ TasksProjectBackend/Controllers/TasksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TasksProjectBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet("example")]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+
+        [HttpGet("example2")]
+        public IActionResult Get2()
+        {
+            return Ok("ok2");
+        }
+
+        [HttpPost("post1")]
+        public IActionResult post1()
+        {
+            return Ok("post1");
+        }
+    }
+}
